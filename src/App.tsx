63d83596c0bc25c00fc2417ddfdc5e45c8076e27
cadetjer <tibullsilverblaze@gmail.com>
2024-05-09 13:58:53 -0400

    fixing name issues in build/src for github

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Experience from "./pages/Experience";
-import Layout from "./Components/layout";
+import Layout from "./Components/leey";
 
 function App() {
   return (
