2605d265cccb22a07333390f9b50233aa834fbd0
cadetjer <tibullsilverblaze@gmail.com>
2024-05-09 02:12:27 -0400

    starting projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,6 +13,8 @@ export default function Projects() {
       <p>
         I did this semester-long project in my Web Development class. This
         website was developed as I learned the concepts covered in class.
+        <br />
+        I used TypeScript, HTML, CSS, and React for the front end. The front end was deployed on Netlify.com. I used Node.js, MongoDB, and Mongoose for the back end. The back end was deployed on Render.com
       </p>
     </div>
   );
